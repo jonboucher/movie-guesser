39212090effef996a44fb75056336e78b46b8050
Jon Boucher <93044611+jonboucher@users.noreply.github.com>
2025-11-17 10:16:00 -0500

    Add movie model

---
+++ src/app/features/guessing-game/models/movie.type.ts
@@ -0,0 +1,10 @@
+export type Movie = {
+  id: number;
+  title: string;
+  release_date: string;
+  vote_average: number;
+  overview: string;
+  cast: string[];
+  genres: string[];
+  tagline: string;
+};
