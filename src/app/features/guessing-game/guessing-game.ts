72d2ff540cbbc2c04efb69211526a094ce1251c4
Jon Boucher <93044611+jonboucher@users.noreply.github.com>
2025-11-17 10:51:01 -0500

    Create placeholder markup

--- src/app/features/guessing-game/guessing-game.ts
+++ src/app/features/guessing-game/guessing-game.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Movie } from './models/movie.type';
 
 @Component({
   selector: 'app-guessing-game',
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './guessing-game.scss',
 })
 export class GuessingGame {
+  guesses = 0;
 
+  movie: Movie = {
+    id: 0,
+    title: 'Star Wars',
+    release_date: '1977',
+    vote_average: 8.6,
+    overview:
+      "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.",
+    cast: ['Mark Hamill', 'Harrison Ford', 'Carrie Fisher'],
+    genres: ['science fiction', 'adventure', 'space opera'],
+    tagline: 'May the force be with you.',
+  };
 }
